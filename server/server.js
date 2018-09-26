3f8e7b914825f3011b0451779afcadd4d1eb17b1
ruchika11 <ruchikasancheti11@gmail.com>
2018-09-26 16:57:32 +0530

    create and new message events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ var io = socketIO(server);
 io.on('connection',(socket)=>{
   console.log("new user connected");
 
+  socket.emit('newMessage',{
+    from: "ruchika",
+    text : "hi ruchika",
+    createdAt : "today"
+  })
+
+  socket.on('createMessage',function(message){
+    console.log(`new message arrives from ${message.from}`);
+    console.log(`message is ${message.text}`);
+  });
+
   socket.on('disconnect',()=>{
     console.log("user was disconnected");
   });
